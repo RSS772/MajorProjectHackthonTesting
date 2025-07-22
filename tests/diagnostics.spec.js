3eea980b92d1f241fb3fd8894570a5900534b4ed
Ritik Sisodiya <ritiksisodiya7610@gmail.com>
2025-07-22 18:21:50 +0530

    initial(test)

--- tests/diagnostics.spec.js
+++ tests/diagnostics.spec.js
@@ -1,14 +1,25 @@
 const { test, expect } = require('@playwright/test');
 const { DiagnosticsPage } = require('../pages/DiagnosticsPage');
 
-
-
-test('@sanity Get top diagnostic cities', async ({ page }) => {
-  const diagnosticsPage = new DiagnosticsPage(page);
-  await diagnosticsPage.goto();
-  const cities = await diagnosticsPage.getCityList();
-  console.log("Cities:", cities);
+const fs = require('fs');
+const path = require('path');
+ 
+test('@regression Get top diagnostic cities and save to JSON', async ({ page }) => {
+  try {
+    const diagnosticsPage = new DiagnosticsPage(page);
+    await diagnosticsPage.goto();
+    const cities = await diagnosticsPage.getCityList();
+ 
+    console.log("Cities:", cities);
+ 
+   
+    const outputPath = path.join(__dirname, '../data/diagnosticCities.json');
+    fs.writeFileSync(outputPath, JSON.stringify({ cities }, null, 2), 'utf-8');
+  } catch (error) {
+    console.log('Error in Get top diagnostic cities test');
+  }
 });
+ 
 
 test('@regression Check for duplicate cities in diagnostics list', async ({ page }) => {
   const diagnosticsPage = new DiagnosticsPage(page);
